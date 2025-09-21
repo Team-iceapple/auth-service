f96a417c7954be64889cf1d1438fac97a8db42ad
Boyeon-Shin <anna007096@gmail.com>
2025-09-21 09:42:48 +0900

    feat: TokenResponse null 값 제외 처리 적용(#1)

--- src/main/java/wisoft/authservice/dto/TokenResponse.java
+++ src/main/java/wisoft/authservice/dto/TokenResponse.java
@@ -1,10 +1,12 @@
 package wisoft.authservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TokenResponse {
     private String accessToken;
     private String message;
